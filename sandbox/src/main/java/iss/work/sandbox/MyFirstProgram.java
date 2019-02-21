1b08ec3abdd4f0d89407baed1eb2ecdbcea53c3a
ktonkov <ktonkov@issart.com>
2019-02-21 17:19:06 +0700

    Second task done

--- sandbox/src/main/java/iss/work/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/iss/work/sandbox/MyFirstProgram.java
@@ -3,5 +3,15 @@ package iss.work.sandbox;
 public class MyFirstProgram {
 	public static void main(String args[]) {
 		System.out.println("Hello, world");
+
+		Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 4);
+
+        System.out.println("Distance between p1 and p2 is " + Distance(p1, p2));
+        System.out.println("Distance between p1 and p2 is " + p1.Distance(p2));
+	}
+
+	private static double Distance(Point p1, Point p2) {
+	    return Math.sqrt(((p1.X() - p2.X()) * (p1.X() - p2.X())) + ((p1.Y() - p2.Y()) * (p1.Y() - p2.Y())));
 	}
 }
---
+++ sandbox/src/main/java/iss/work/sandbox/Point.java
@@ -0,0 +1,23 @@
+package iss.work.sandbox;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int X() {
+        return x;
+    }
+
+    public int Y() {
+        return y;
+    }
+
+    public double Distance(Point p) {
+        return Math.sqrt(((x - p.X()) * (x - p.X())) + ((y - p.Y()) * (y - p.Y())));
+    }
+}
